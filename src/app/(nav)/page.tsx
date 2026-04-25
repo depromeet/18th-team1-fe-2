38a7c8a2c38e15225e935c42c58f4d34bc9920b7
nakyeong <yunna3425@naver.com>
2026-04-25 21:51:56 +0900

    fix: 변경된 버튼 props 적용 (#9)

--- src/app/(nav)/page.tsx
+++ src/app/(nav)/page.tsx
@@ -9,7 +9,7 @@ const HomePage = (): React.ReactElement => {
     <div className="flex flex-col items-center gap-10 py-10">
       <section className="flex flex-col items-center gap-3">
         <Button label="다음" />
-        <Button label="다음" disabled />
+        <Button label="다음" isDisabled />
         <Button label="오늘의 문장 작성하기" icon={<IcPlus size={11} className="text-gray-0" />} />
         <ButtonDouble secondaryLabel="취소" primaryLabel="확인" />
         <ButtonWithIcon primaryLabel="공유하기" />
