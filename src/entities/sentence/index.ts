483e8db509aec9764e9475092634709ffe35bd6d
cindy-chaewon <chaewon1769@daum.net>
2026-05-03 02:29:44 +0900

    refactor: SentenceTextCard 엔티티에서 제거 (#21)

--- src/entities/sentence/index.ts
+++ src/entities/sentence/index.ts
@@ -2,4 +2,3 @@ export type { RecommendedSentence } from "./model/sentence.types";
 export { SentenceCard } from "./ui/SentenceCard";
 export { SentenceListCard } from "./ui/SentenceListCard";
 export { SentenceListCardSkeleton } from "./ui/SentenceListCardSkeleton";
-export { SentenceTextCard } from "./ui/SentenceTextCard";
